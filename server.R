8a81d82147d0e37bd6f01b750d7b0416ed5b450f
joaopoeta <joao.o.poeta@gmail.com>
2015-05-16 20:58:34 +0200

    envio do arquivo regression model

---
+++ server.R
@@ -0,0 +1,119 @@
+library(shiny)
+library(UsingR)
+
+shinyServer(
+        function(input, output) { 
+                
+                output$newHist <- renderPlot({
+                        # Get Inputs
+                        set.seed(input$seed)
+                        N <- input$N
+                        range <- input$range
+                        delta <- input$delta
+                       
+                        # Generating Random Numbers From a Linear Model
+                        x <- rnorm(N)
+                        error <- rnorm(N, 0, 2.5) 
+                        y <- 0.4 + 3*x + error 
+
+                        # Running the Regression
+                        model <- lm(y ~ x)
+                        
+                        # Graphics elements
+                        plot(x, y, main="Linear Regression Solution vs. Arbitrary Straight Line")
+                        abline(model, lwd=3, col="blue")
+                        
+                        # Constructing the dynamic line
+                        a <- min(x) - 5
+                        c <- max(x) + 5
+                        b <- predict(model, newdata = data.frame(x = a))-delta
+                        d <- predict(model, newdata = data.frame(x = c))+delta
+                        
+                        seqX <- seq(a, c, length.out = 1000)
+                        seqY <- ( (d-b)*seqX + (c*b-a*d) ) / (c-a)
+        
+                        points(c(a, c), c(b,d), type='l', lwd=1, col="red")
+                        
+                        if (input$legend)
+                        {
+                                legend("bottomright",c("Linear Regression Solution","Arbitrary Straight Line"), lty=c(1,1), lwd=c(3,1),col=c("blue","red")) 
+                        }
+                        
+                        vertical <- switch(input$vertical, l0 = 0, l1 = 1, l2 = 2, 0)
+                        
+                        if (vertical == 1) 
+                        {       # Show Verticals to Arbitrary Line:
+                                s <- seq(length(x))
+                                indexClosest <- c()
+                                for(i in s){ indexClosest <- c(indexClosest, which.min(abs(seqX - x[i])) ) }
+                                segments(x[s], y[s], x[s], seqY[indexClosest], col= 'forestgreen')
+                        }
+               
+                        if (vertical == 2) 
+                        {       # Show Verticals to LR line:
+                                s <- seq(length(x))
+                                predicY <- predict(model, newdata = data.frame(x = x))
+                                segments(x[s], y[s], x[s], predicY, col= 'forestgreen')
+                        }
+                        
+                        })
+
+                # Table
+                distributionValues <- reactive({
+                          
+                        # Get Inputs
+                        set.seed(input$seed)
+                        N <- input$N
+                        range <- input$range
+                        delta <- input$delta
+                        
+                        # Generating Random Numbers From a Linear Model
+                        x <- rnorm(N)
+                        error <- rnorm(N, 0, 2) 
+                        y <- 0.5 + 2*x + error 
+                        model <- lm(y ~ x)
+                        
+                        # Residual Error Arbitrary Line
+                        a <- min(x) - 5
+                        c <- max(x) + 5
+                        b <- predict(model, newdata = data.frame(x = a))-delta
+                        d <- predict(model, newdata = data.frame(x = c))+delta
+                        
+                        seqX <- seq(a, c, length.out = 1000)
+                        seqY <- ( (d-b)*seqX + (c*b-a*d) ) / (c-a)
+                        s <- seq(length(x))
+                        indexClosest <- c()
+                        residAuxiliar <- 0
+                        for(i in s)
+                        { 
+                                indexClosest <- c(indexClosest, which.min(abs(seqX - x[i])) ) 
+                                residAuxiliar <- residAuxiliar + (seqY[indexClosest[i]]-y[i])^2
+                        }
+                        resArbitrary <- sqrt(residAuxiliar / (N - 2))
+                        
+                        # Get Statistics
+                        fstatistic <- summary(model)[[10]][[1]]
+                        p1 <- summary(model)[[4]][[8]]
+                        resLM <- summary(model)$sigma 
+                        
+                        # Compose data frame
+                        data.frame(Name = c("Correlation (x,y)", 
+                                            "Covariance (x,y)",
+                                            "Residual Linear Model",
+                                            "Residual Arbitrary"),
+                                   
+                                   Value = as.character(c(signif(cor(x,y)), 
+                                                          signif(cov(x,y)), 
+                                                          signif(resLM),
+                                                          signif(resArbitrary)),digits=4),
+                                   stringsAsFactors=FALSE)
+      
+                })
+                
+                # Show the table
+                output$resume <- renderTable({
+                        distributionValues()
+                })
+
+})
+
---
+++ ui.R
@@ -0,0 +1,21 @@
+library(shiny)
+shinyUI(pageWithSidebar(
+    
+        headerPanel("Linear Regression with Random Numbers"),
+        
+        sidebarPanel(
+                sliderInput('N', 'Samples size:',value = 5, min = 3, max = 30, step = 1),
+                numericInput('delta', 'Arbitrary Straight Line Generator:', value = 0, min = -50, max = 50, step = 1),
+                radioButtons('vertical', 'Vertical Line Control:', c("Without Verticals Distances" = "l0",  "Show Verticals to Arbitrary Line" = "l1", "Show Verticals to LR line Solution" = "l2"), inline = FALSE),
+                checkboxInput(inputId = "legend",label = strong("Show Legend"),value = TRUE),
+                selectizeInput('seed', 'Seed to generate random numbers:', choices = c(1, 42, 77, 121),selected = 1)
+                ),
+        
+        mainPanel(
+                h4("Plots:"),
+                plotOutput('newHist'),
+          
+                h4("Resume:"),
+                tableOutput("resume")
+  )
+))
